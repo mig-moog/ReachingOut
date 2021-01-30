6d3965910c8fe5a05d22b74376f60389bf7ae551
alexw-1 <77253282+alexw-1@users.noreply.github.com>
2021-01-30 18:57:18 -0500

    contacts

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import insta from './insta2.png'
+import insta from './insta3.jpg'
 class Contacts extends React.Component{
     render(){
         return(
             <div>
-                <h2>Follow us on Instagram!</h2>
-                <img alt='instagram profile' src={insta}></img>
+                <a target="_blank" href='https://www.instagram.com/reachingoutlaptops/?hl=en'>Follow us on Instagram!</a>
+                <img id='instalogo' alt='instagram profile' src={insta}></img>
+                <br />
+                <br />
+                <br />
                 <h3>If you have any questions please email: reachoutlaptops@gmail.com</h3>
             </div>
         );
