2a4d6e23350525ae6bbee9bb2c1204bdc68fae2a
Matthew Hirsch <hirschmatthew9@gmail.com>
2021-01-30 11:04:00 -0500

    NavBar start

--- src/App.js
+++ src/App.js
@@ -3,38 +3,28 @@ import './App.css';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
 } from "react-router-dom";
 import Home from './Home'
 import About from "./About"
 import Contacts from './Contacts';
+
+import NavBar from "./NavBar"
+
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/home'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/contacts'>Contacts</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavBar/>
     <Switch>
-        <Route path="/home">
-          <Home></Home>
-        </Route>
         <Route path="/about">
           <About></About>
         </Route>
         <Route path="/contacts">
           <Contacts></Contacts>
+        </Route>
+        <Route path="/">
+          <Home></Home>
         </Route>
     </Switch>
       </div>
