146223ef5a556249f345da3194c8eb9108675e46
alexw-1 <77253282+alexw-1@users.noreply.github.com>
2021-01-31 10:54:24 -0500

    more pictures

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import laptop from './laptop.jpg';
+import router from './router.jpg'
 class Home extends React.Component {
     render() {
         return(
@@ -7,7 +8,10 @@ class Home extends React.Component {
                    <h1 id='reaching_out'>Reaching Out</h1>
                    <p>Our mission statement: Our mission is to enhance and enrich the lives of people in developing nations by providing them with access to the world wide web. We achieve this through setting up computer labs within these towns and villages and lending donated and purchased laptops. Through these machines and their newfound access to the internet, they can educate themselves through free online resources and gain new career skills and knowledge.</p>
                    <img id='the_laptop' alt="laptop" src={laptop}></img>
-                   <p>Reaching Out is a non-profit company that allows many people to have access to the internet who otherwise wouldn't be able to. Our service will provide free internet and laptops to people in less wealthy areas such as Africa and the Middle East. In these places internet is far less common than in first world countries like the United States. In the 21st century, the world's modern economy is steadily moving towards technology focused jobs. By providing technology to these developing countries, we can help their young ones gain valuable technology experience which they might not have otherwise received. This will help the economies of these less developed and developing countries become more modern.</p>
+                   <p>Reaching Out is a non-profit company that allows many people to have access to the internet who otherwise wouldn't be able to. Our service will provide free internet and laptops to people in less wealthy areas such as Africa and the Middle East. In these places internet is far less common than in first world countries like the United States. </p>
+                   <img alt='router' src={router}></img>
+                   <p>In the 21st century, the world's modern economy is steadily moving towards technology focused jobs. By providing technology to these developing countries, we can help their young ones gain valuable technology experience which they might not have otherwise received. This will help the economies of these less developed and developing countries become more modern.</p>
+                   <br />
                    <a href='/Internet_center.stl' download>Click to download a 3d model of a laptop distribution area</a>
                </div>
         );
--- src/future.js
+++ src/future.js
@@ -3,7 +3,7 @@ import sat from './sat.png';
 class Future extends React.Component{
     render(){
         return(
-            <div id='text_page'>
+            <div id='text_page2'>
                 <h1 id='future_header'>Our plans for the future</h1>
                 <p>In the near future, satellites will be able to beam down internet to almost anywhere on the Earth's surface. This will allow us to connect to the internet from many places where we otherwise not be able to connect. This will allow us to give so many young people internet access. With satellites we will not have to build lots of infastructure to connect these people to the world wide web.</p>
                 <img id='satellite' alt='satellite' src={sat}></img>
